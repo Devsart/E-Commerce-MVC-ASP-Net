5898321c028407818a31808310c888d6a40a7030
Matheus Lira Sartor <matheus.sartor@radixeng.com.br>
2021-01-28 16:35:09 -0300

    Adicionar arquivos de projeto.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class BaseRepository<T> where T : BaseModel
+    {
+        public readonly ApplicationContext contexto;
+        public readonly DbSet<T> dbSet;
+
+        public BaseRepository(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+            dbSet = contexto.Set<T>();
+        }
+    }
+}
